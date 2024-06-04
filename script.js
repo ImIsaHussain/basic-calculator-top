4e18413d2ffb7bcf78e3c3af70c544606ebc2c89
Isa Hussain <106785348+ImIsaHussain@users.noreply.github.com>
2024-06-04 14:10:38 -0400

    Complete calculator project 1.0
    No decimal point functionality yet
    No percentage functionality yet
    No use previous result functionality yet

--- script.js
+++ script.js
@@ -1,53 +1,130 @@
 const display = document.getElementById("display");
+const historyContainer = document.getElementById("history-list");
 
 document.addEventListener("DOMContentLoaded", function() {
     const selection = document.querySelectorAll("button");
-    
+    let numOne = "";
+    let numTwo = "";
+    let sign;
+    let isSign = false;
+    let equals = false;
+
     selection.forEach((button) => {
         button.addEventListener("click", () => {
-            console.log(button.id);
+
+            if (["multiply", "divide", "add", "subtract", "equals", "decimal"].includes(button.id)) {
+                
+            } else if (sign === undefined) {
+                numOne += button.id;
+                console.log(numOne);
+            } else {
+                numTwo += button.id;
+                console.log(numTwo);
+            }
+
+            if (["multiply", "divide", "add", "subtract"].includes(button.id)) {
+                sign = button.id;
+                isSign = true;
+                console.log(sign);
+            }
 
             switch (button.id) {
                 case "clear":
                     display.textContent = "";
+                    numOne = "";
+                    numTwo = "";
+                    sign = undefined;
                     break;
-                case "zero":
-                    if (display.textContent === "" || display.textContent === "0") {
-                        display.textContent = 0;
+                case "0":
+                    if (display.textContent === "" || display.textContent === "0" || equals === true || isSign === true) {
+                        display.textContent = "0";
+                        isSign = false;
+                        equals = false;
                     } else {
-                        display.textContent += 0;
+                        display.textContent += "0";
                     }
                     break;
-                case "one":
-                    display.textContent += 1;
+                case "1":
+                    if (display.textContent === "" || display.textContent === "0" || equals === true || isSign === true) {
+                        display.textContent = "1";
+                        isSign = false;
+                        equals = false;
+                    } else {
+                        display.textContent += "1";
+                    }
                     break;
-                case "two":
-                    display.textContent += 2;
+                case "2":
+                    if (display.textContent === "" || display.textContent === "0" || equals === true || isSign === true) {
+                        display.textContent = "2";
+                        isSign = false;
+                        equals = false;
+                    } else {
+                        display.textContent += "2";
+                    }
                     break;
-                case "three":
-                    display.textContent += 3;
+                case "3":
+                    if (display.textContent === "" || display.textContent === "0" || equals === true || isSign === true) {
+                        display.textContent = "3";
+                        isSign = false;
+                        equals = false;
+                    } else {
+                        display.textContent += "3";
+                    }
                     break;
-                case "four":
-                    display.textContent += 4;
+                case "4":
+                    if (display.textContent === "" || display.textContent === "0" || equals === true || isSign === true) {
+                        display.textContent = "4";
+                        isSign = false;
+                        equals = false;
+                    } else {
+                        display.textContent += "4";
+                    }
                     break;
-                case "five":
-                    display.textContent += 5;
+                case "5":
+                    if (display.textContent === "" || display.textContent === "0" || equals === true || isSign === true) {
+                        display.textContent = "5";
+                        isSign = false;
+                        equals = false;
+                    } else {
+                        display.textContent += "5";
+                    }
                     break;
-                case "six":
-                    display.textContent += 6;
+                case "6":
+                    if (display.textContent === "" || display.textContent === "0" || equals === true || isSign === true) {
+                        display.textContent = "6";
+                        isSign = false;
+                        equals = false;
+                    } else {
+                        display.textContent += "6";
+                    }
                     break;
-                case "seven":
-                    display.textContent += 7;
+                case "7":
+                    if (display.textContent === "" || display.textContent === "0" || equals === true || isSign === true) {
+                        display.textContent = "7";
+                        isSign = false;
+                        equals = false;
+                    } else {
+                        display.textContent += "7";
+                    }
                     break;
-                case "eight":
-                    display.textContent += 8;
+                case "8":
+                    if (display.textContent === "" || display.textContent === "0" || equals === true || isSign === true) {
+                        display.textContent = "8";
+                        isSign = false;
+                        equals = false;
+                    } else {
+                        display.textContent += "8";
+                    }
                     break;
-                case "nine":
-                    display.textContent += 9;
+                case "9":
+                    if (display.textContent === "" || display.textContent === "0" || equals === true || isSign === true) {
+                        display.textContent = "9";
+                        isSign = false;
+                        equals = false;
+                    } else {
+                        display.textContent += "9";
+                    }
                     break;
-                case "equals":
-                    display.textContent = "=";
-                    break;  
                 case "multiply":
                     display.textContent = "X";
                     break;
@@ -59,34 +136,70 @@ document.addEventListener("DOMContentLoaded", function() {
                     break;
                 case "subtract":
                     display.textContent = "-";
+                    break;
+                // case "decimal":
+                //     display.textContent += ".";
+                //     break;
+                case "equals":
+                    equals = true;
+                    display.textContent = operate(numOne, numTwo, sign);
+                    history(numOne, numTwo, sign);
+                    numOne = "";
+                    numTwo = "";
+                    sign = undefined;
                     break;                                                              
             }
 
             if (button.id === "clear") {
                 display.textContent = "";
+                numOne = "";
+                numTwo = "";
+                sign = undefined;
             } 
         });
     });
-
-    
 });
 
+function history(num1, num2, sign) {
+    switch (sign) {
+        case 'add':
+            sign = " + ";
+            break;
+        case 'subtract':
+            sign = " - ";
+            break;
+        case 'multiply':
+            sign = " x ";
+            break;
+        case 'divide':
+            sign = " / ";
+            break;
+    }
 
-
-
-
+    const newHistory = document.createElement("p");
+    newHistory.classList.add("history-item");
+    newHistory.textContent = num1 + sign + num2 + " = " + result;
+    historyContainer.insertBefore(newHistory, historyContainer.firstChild);
+}
 
 function operate (num1, num2, sign) {
+    numOne = parseInt(num1);
+    numTwo = parseInt(num2);
+
     switch (sign) {
-        case '+':
-            return num1 + num2;
-        case '-':
-            return num1 - num2;
-        case 'x':
-            return num1 * num2;
-        case '/':
-            return num1 / num2;
-        default:
-            return 'Invalid operator';
+        case 'add':
+            result = numOne + numTwo;
+            break;
+        case 'subtract':
+            result = numOne - numTwo;
+            break;
+        case 'multiply':
+            result = numOne * numTwo;
+            break;
+        case 'divide':
+            result = numOne / numTwo;
+            break;
     }
+    console.log(result);
+    return result;
 }
